1a5cd1144b5e158d21ce82609829ebeaba58942a
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2025-05-14 15:11:20 +0200

    docs

--- cmd/docscmd/docscmd.go
+++ cmd/docscmd/docscmd.go
@@ -9,17 +9,17 @@ import (
 )
 
 func New(rootCmd *cobra.Command, path string) *cobra.Command {
-	if _, err := os.Stat(path); err != nil {
-		if os.IsNotExist(err) {
-			if err := os.MkdirAll(path, os.ModePerm); err != nil {
-				cobra.CheckErr(fmt.Errorf(`error creating directory "%s": %w`, path, err))
-			}
-		}
-	}
 	return &cobra.Command{
 		Use:   "docs",
 		Short: "Generate documentation files",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if _, err := os.Stat(path); err != nil {
+				if os.IsNotExist(err) {
+					if err := os.MkdirAll(path, os.ModePerm); err != nil {
+						return fmt.Errorf(`error creating directory "%s": %w`, path, err)
+					}
+				}
+			}
 			return doc.GenMarkdownTree(rootCmd, path)
 		},
 	}
